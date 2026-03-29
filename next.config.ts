1ba9f6f3b1aedfd56189b48de2865eb1f8af3b4c
Sadeepa Tharushan <sadeepatharushan158@gmail.com>
2026-03-29 14:46:15 +0530

    data base and prisma updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
-
+/*
 const nextConfig: NextConfig = {
   // Only include the IP if it's defined in your local environment
+  allowedDevOrigins: process.env.ALLOWED_DEV_IP ? [process.env.ALLOWED_DEV_IP] : [],
+};
+
+export default nextConfig;
+*/
+
+const nextConfig = {
+  // This reads the IP from your .env file
   allowedDevOrigins: process.env.ALLOWED_DEV_IP ? [process.env.ALLOWED_DEV_IP] : [],
 };
 
